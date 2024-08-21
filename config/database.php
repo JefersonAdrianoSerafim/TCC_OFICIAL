24e16e71fb5454b0916dd4ea8875d74328b6b4bd
Jeferson Adriano Serafim Filho <jefersonadrianoserafimfilho@gmail.com>
2024-08-21 16:51:31 -0300

    configuração inicial env oficial

--- config/database.php
+++ config/database.php
@@ -116,7 +116,7 @@ return [
 
             'driver' => 'mongodb',
             'host' => env('MONGODB_HOST', '127.0.0.1'),
-            'port' => env('MONGODB_PORT', 30000),
+            'port' => env('MONGODB_PORT', 27017),
             'database' => env('MONGODB_DATABASE'),
             'username'=> env('MONGODB_USERNAME'),
             'password'=> env('MONGODB_PASSWORD'),
