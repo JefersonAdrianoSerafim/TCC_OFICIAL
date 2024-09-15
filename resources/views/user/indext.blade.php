1f05d8b08c706f35dad2f996dfd2b7989db5cbbe
Jeferson Adriano Serafim Filho <jefersonadrianoserafimfilho@gmail.com>
2024-09-15 16:49:00 -0300

    finalização navBar Menu

--- resources/views/user/indext.blade.php
+++ resources/views/user/indext.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu do Usuário - SAEV, Sistema de Agenda Escolar Virtual</title>
     <link rel="stylesheet" href="css/style_menu.css">
+    <script src="https://kit.fontawesome.com/ef84d19b33.js" crossorigin="anonymous"></script>
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
@@ -14,12 +15,46 @@
 </head>
 
 <body>
-    <div class="container">
-        <a href="" class="" toggleBox>
-            <span class="icon"></span>
-        </a>
-    </div>
-    <script src="js/menu_script.js"></script>
+    <nav>
+        <ul>
+            <li>
+                <a href="#" class="logo">
+                    <i class="fa-solid fa-book"></i>
+                    <span class="nav-item">MENU</span>
+                </a>
+            </li>
+            <li><a href="#">
+                    <i class="fa-solid fa-house"></i>
+                    <span class="nav-item">Home</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="fa-solid fa-user"></i>
+                    <span class="nav-item">Perfil</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span class="nav-item">Agenda</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="fa-solid fa-chart-column"></i>
+                    <span class="nav-item">Gráficos</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="fa-solid fa-gear"></i>
+                    <span class="nav-item">Configurações</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="fa-solid fa-circle-question"></i>
+                    <span class="nav-item">Ajuda</span>
+                </a></li>
+            <li><a href="#" class="logout">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                    <span class="nav-item">Sair</span>
+                </a></li>
+
+        </ul>
+
+    </nav>
 </body>
 
 </html>
