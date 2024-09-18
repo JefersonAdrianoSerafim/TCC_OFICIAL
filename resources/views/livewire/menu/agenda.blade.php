41905db1fd70d185cb7ee101886e651de2fbdcd2
Jeferson Adriano Serafim Filho <jefersonadrianoserafimfilho@gmail.com>
2024-09-18 10:53:06 -0300

    finalização para TCS

--- resources/views/livewire/menu/agenda.blade.php
+++ resources/views/livewire/menu/agenda.blade.php
@@ -3,48 +3,52 @@
         @if ($activeComponentAgendaL == 'create')
             <h1 class="">Lista de compromissos</h1>
 
-        @if(App\Http\Controllers\UserController::verifyIfCommitmentNull())
-            <p> Sem compromissos marcados!! </p>
-        @else
-            <table>
-                <thead class="theadTable">
-                    <tr class="trTable">
-                        <th id="thName">Nome</th>
-                        <th id="thDescription">Descrição</th>
-                        <th id="thSubject"> Materia</th>
-                        <th id="thAction"> Ações</th>
-                    </tr>
-                </thead>
-                <tbody class="tbodyTable">
-                    @foreach(auth()->user()->subjects as $subject)
+            @if(App\Http\Controllers\UserController::verifyIfCommitmentNull())
+                <p> Sem compromissos marcados!! </p>
+            @else
+                <table>
+                    <thead class="theadTable">
+                        <tr class="trTable">
+                            <th id="thName">Nome</th>
+                            <th id="thDescription">Descrição</th>
+                            <th id="thSubject"> Materia</th>
+                            <th id="thAction"> Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="tbodyTable">
+                        @foreach(auth()->user()->subjects as $subject)
 
-                        @foreach($subject->commitments as $commitment)
-                            <tr>
-                                <td id="tdNameCommitment">{{ $commitment->name_commitment }}</td>
-                                <td id="tdDescription"> {{ $commitment->description_commitment }} </td>
-                                <td id="tdNameSubject"> {{$subject->name_subject}}</td>
-                                <td>
-                                <button class="editButton" wire:click="setActiveComponentAgendaL('edit', {{ $commitment->id }})">Editar</button>
-                                <form  method="POST" action="{{ route('commitment.destroy', $commitment->id) }}">
-                                @csrf
-                                @method('DELETE')
-                                    <button class="deleteButton" type="submit">Excluir</button>
-                                </form>
-                                </td>
-                            </tr>
-                            
+                            @foreach($subject->commitments as $commitment)
+                                <tr>
+                                    <td id="tdNameCommitment">{{ $commitment->name_commitment }}</td>
+                                    <td id="tdDescription"> {{ $commitment->description_commitment }} </td>
+                                    <td id="tdNameSubject"> {{$subject->name_subject}}</td>
+                                    <td>
+                                        <div class="buttons">
+                                            <button class="editButton"
+                                                wire:click="setActiveComponentAgendaL('edit', {{ $commitment->id }})">Editar</button>
+                                            <form method="POST" action="{{ route('commitment.destroy', $commitment->id) }}">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="deleteButton" type="submit">Excluir</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            @endforeach
                         @endforeach
-                    @endforeach
-                    
-                </tbody>
-            </table>
-        @endif
+
+                    </tbody>
+                </table>
+            @endif
             <div class="containerCreate">
                 <h1>Adicionar compromisso</h1>
                 <form method="POST" action=" {{route('commitment.store')}}">
                     @csrf
                     <input type="text" name="name_commitment" id="" placeholder="Nome" value="{{old('name_commitment')}}">
-                    <input type="text" name="description_commitment" id="" placeholder="Descrição" value="{{old('description_commitment')}}" >
+                    <input type="text" name="description_commitment" id="" placeholder="Descrição"
+                        value="{{old('description_commitment')}}">
                     <select name="subject_commitment">
                         <option selected disabled></option>
                         @foreach (auth()->user()->subjects as $subject)
@@ -54,20 +58,23 @@
                     @if($errors->any())
                         <div class='invalid-feedback'>
                             <ul>
-                                    <li> {{ $errors->first() }}</li>
+                                <li> {{ $errors->first() }}</li>
                             </ul>
                         </div>
                     @endif
-                    <button class="btn" id="submitRegister" type="submit">Criar Compromisso</button>
+                    <button class="createButton" id="submitRegister" type="submit">Criar Compromisso</button>
                 </form>
             </div>
 
         @elseif ($activeComponentAgendaL == 'edit')
-            <form method="POST" action=" {{route('commitment.update', $selected_commitment->id)}}">
+            <div class="containerEdit">
+                <form method="POST" action=" {{route('commitment.update', $selected_commitment->id)}}">
                     @csrf
                     @method('PUT')
-                    <input type="text" name="name_commitment" id="" placeholder="Nome" value="{{$selected_commitment->name_commitment}}">
-                    <input type="text" name="description_commitment" id="" placeholder="Descrição" value="{{$selected_commitment->description_commitment }}" >
+                    <input type="text" name="name_commitment" id="" placeholder="Nome"
+                        value="{{$selected_commitment->name_commitment}}">
+                    <input type="text" name="description_commitment" id="" placeholder="Descrição"
+                        value="{{$selected_commitment->description_commitment }}">
                     <select name="subject_commitment">
                         <option selected value="{{$selected_commitment->subjects->name_subject }}"></option>
                         @foreach (auth()->user()->subjects as $subject)
@@ -77,89 +84,99 @@
                     @if($errors->any())
                         <div class='invalid-feedback'>
                             <ul>
-                                    <li> {{ $errors->first() }}</li>
+                                <li> {{ $errors->first() }}</li>
                             </ul>
                         </div>
                     @endif
-                    <button class="btn" type="submit" id="voltar" wire:click="setActiveComponentAgendaL('create', 0)">Editar Compromisso</button>
+                    <button class="editButton" type="submit" id="voltar" wire:click="setActiveComponentAgendaL('create', 0)">Editar
+                        Compromisso</button>
                 </form>
-                <button class="createButton" wire:click=" setActiveComponentAgendaL('create')">Voltar</button>
+                <button class="createButton" wire:click=" setActiveComponentAgendaL('create', 0)">Voltar</button>
+            </div>
+
         @endif
     </div>
     <div class="containerRightAgenda">
-        
+
 
         @if ($activeSubjectAgendaR == 'create')
-        <h1 class="">Lista de matérias</h1>
+            <h1>Lista de disciplinas</h1>
 
-@if(auth()->user()->subjects->isEmpty())
-    <p> Sem matérias encontrados </p>
-@else
-    <table>
-        <thead class="theadTable">
-            <tr class="trTable">
-                <th>Nome</th>
-                <th>Professor</th>
-                <th>
-
-                </th>
-            </tr>
-        </thead>
-        <tbody class="tbodyTable">
-            @foreach(auth()->user()->subjects as $subject)
-                <tr>
-                    <td>{{ $subject->name_subject }}</td>
-                    <td> {{ $subject->teacher_subject }} </td>
-                    <td>
-                        <div class="buttons">
-                        <button class="editButton" wire:click=" setActiveSubjectAgendaR('edit', {{$subject->id}})">Editar</button>
-                        <form  method="POST" action="{{ route('subject.destroy', $subject->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button class="deleteButton "type="submit">Excluir</button>
-                        </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endif
+            @if(auth()->user()->subjects->isEmpty())
+                <p> Sem disciplinas encontrados </p>
+            @else
+                <table>
+                    <thead class="theadTable">
+                        <tr class="trTable">
+                            <th>Nome</th>
+                            <th>Professor</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="tbodyTable">
+                        @foreach(auth()->user()->subjects as $subject)
+                            <tr>
+                                <td>{{ $subject->name_subject }}</td>
+                                <td> {{ $subject->teacher_subject }} </td>
+                                <td>
+                                    <div class="buttons">
+                                        <button class="editButton"
+                                            wire:click=" setActiveSubjectAgendaR('edit', {{$subject->id}})">Editar</button>
+                                        <form method="POST" action="{{ route('subject.destroy', $subject->id) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="deleteButton " type="submit">Excluir</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
             <div class="containerCreate">
                 <h1>Adicionar disciplina</h1>
                 <form method="POST" action="{{route('subject.store')}}">
                     @csrf
-                    <input type="text" name="name_subject" id="" placeholder="Nome" value="{{ old('name_subject') }}" >
-                    <input type="text" name="teacher_subject" id="" placeholder="Professor" value="{{ old('teacher_subject') }}">
+                    <input type="text" name="name_subject" id="" placeholder="Nome" value="{{ old('name_subject') }}">
+                    <input type="text" name="teacher_subject" id="" placeholder="Professor"
+                        value="{{ old('teacher_subject') }}">
                     @if($errors->any())
                         <div class='invalid-feedback'>
-                        @foreach($errors as $error)
-                            <ul>
-                                    <li> {{ $error }}</li>
-                            </ul>
-                        </div>
-                        @endforeach
+                            @foreach($errors as $error)
+                                    <ul>
+                                        <li> {{ $error }}</li>
+                                    </ul>
+                                </div>
+                            @endforeach
                     @endif
-                    <button class="btn" id="submitRegister" type="submit">Criar Compromisso</button>
+                    <button class="createButton" id="submitRegister" type="submit">Criar matéria</button>
                 </form>
-                <button class="btn" id="submitRegister" type="submit">Voltar</button>
             </div>
         @elseif ($activeSubjectAgendaR == 'edit')
-            <form method="POST" action=" {{route('subject.update', $selected_subject->id)}}">
-                @csrf
-                @method('PUT')
-                <input type="text" name="name_subject" id="" placeholder="Nome" value="{{$selected_subject->name_subject}}">
-                <input type="text" name="teacher_subject" id="" placeholder="Descrição" value="{{$selected_subject->teacher_subject }}" >
-                @if($errors->any())
-                    <div class='invalid-feedback'>
-                        <ul>
-                            <li> {{ $errors->first() }}</li>
-                        </ul>
-                    </div>
-                @endif
-                <button class="btn" type="submit" id="voltar" wire:click="setActiveSubjectAgendaR('create', 0)">Editar Matéria</button>
-            </form>
-            <button class="createButton" wire:click=" setActiveSubjectAgendaR('create, 0')">Cancelar</button>
+            <div class="containerEdit">
+                <form method="POST" action=" {{route('subject.update', $selected_subject->id)}}">
+                    @csrf
+                    @method('PUT')
+                    <input type="text" name="name_subject" id="" placeholder="Nome"
+                        value="{{$selected_subject->name_subject}}">
+                    <input type="text" name="teacher_subject" id="" placeholder="Descrição"
+                        value="{{$selected_subject->teacher_subject }}">
+                    @if($errors->any())
+                        <div class='invalid-feedback'>
+                            <ul>
+                                <li> {{ $errors->first() }}</li>
+                            </ul>
+                        </div>
+                    @endif
+                    <button class="editButton" type="submit" id="voltar"
+                        wire:click="setActiveSubjectAgendaR('create', 0)">Editar Matéria</button>
+
+                </form>
+                <button class="createButton" wire:click=" setActiveSubjectAgendaR('create', 0)">Voltar</button>
+
+            </div>
+
         @endif
     </div>
 
--- resources/views/livewire/menu/team.blade.php
+++ resources/views/livewire/menu/team.blade.php
@@ -1,59 +1,58 @@
 <div class="containerActive">
-    <div class="containerTeam">
-        <h1 class="">Lista de Turmas</h1>
-
-        @if(auth()->user()->teams->isEmpty())
-            <p> Você não está em nenhuma turma. </p>
-        @else
-            <table>
-                <thead class="theadTable">
-                    <tr class="trTable">
-                        <th id="thId">Link</th>
-                        <th id="thName">Nome</th>
-                        <th id="thDescription">Cor</th>
-                        <th id="thActions">Ações</th>
-                    </tr>
-                </thead>
-                <tbody class="tbodyTable">
-                    @foreach(auth()->user()->teams as $team)
-                        <tr>
-                            <td> localhost:8000/user/team/enter/{{$team->id}}/{{Hash::make($team->id) }}</td>
-                            <td>{{$team->name_team }}</td>
-                            <td style="color:{{$team->color_team}};"> {{ $team->color_team }}</td>
-                            <td>
-                                <div class ="buttons">
-                                    <button class="editButton" wire:click=" setActiveGroupTeamL('edit', {{$team->id}} )">Editar</button>
-                                    <form  method="POST" action="{{ route('team.destroy', $team->id) }}">
-                                    @csrf
-                                    @method('DELETE')
-                                        <button class="deleteButton" type="submit">Excluir</button>
-                                    </form>
-                                </div>
-                                
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
-
+    <div class="containerLeftTeam">
         @if ($activeGroupTeamL == 'create')
+            <h1>Lista de Turmas</h1>
+
+            @if(auth()->user()->teams->isEmpty())
+                <p> Você não está em nenhuma turma. </p>
+            @else
+                <table>
+                    <thead class="theadTable">
+                        <tr class="trTable">
+                            <th id="thId">Link</th>
+                            <th id="thName">Nome</th>
+                            <th id="thDescription">Cor</th>
+                            <th id="thActions">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody class="tbodyTable">
+                        @foreach(auth()->user()->teams as $team)
+                            <tr>
+                                <td> localhost:8000/user/team/enter/{{$team->id}}/{{Hash::make($team->id) }}</td>
+                                <td>{{$team->name_team }}</td>
+                                <td style="background-color:{{$team->color_team}};"></td>
+                                <td>
+                                    <div class="buttons">
+                                        <button class="editButton"
+                                            wire:click=" setActiveGroupTeamL('edit', {{$team->id}} )">Editar</button>
+                                        <form method="POST" action="{{ route('team.destroy', $team->id) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="deleteButton" type="submit">Excluir</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
             <div class="containerCreate">
-                <h1>Criar</h1>
+                <h1>Criar turma</h1>
                 <form method="POST" action="{{route('team.store')}}">
-                @csrf
+                    @csrf
                     <input type="text" name="name_team" id="" placeholder="Nome">
                     <input type="color" name="color_team" id="" placeholder="Cor">
                     <button class="createButton" type="input">Criar</button>
                 </form>
             </div>
-
         @elseif ($activeGroupTeamL == 'edit')
-            <form method="POST" action=" {{route('team.update', $selected_team->id)}}">
+            <div class="containerEdit">
+                <form method="POST" action=" {{route('team.update', $selected_team->id)}}">
                     @csrf
                     @method('PUT')
                     <input type="text" name="name_team" id="" placeholder="Nome" value="{{$selected_team->name_team}}">
-                    <input type="color" name="color_team" id="" value="{{$selected_team->description_team }}" >
+                    <input type="color" name="color_team" id="" value="{{$selected_team->description_team }}">
                     @if($errors->any())
                         <div class='invalid-feedback'>
                             <ul>
@@ -61,12 +60,24 @@
                             </ul>
                         </div>
                     @endif
-                    <button class="btn" type="submit" id="voltar" wire:click="setActiveGroupTeamL('create', 0)">Editar Grupo</button>
+                    <div class="buttons">
+                        <button class="editButton" type="submit" id="voltar"
+                            wire:click="setActiveGroupTeamL('create', 0)">Editar
+                        </button>
+                    </div>
+                    <button class="createButton" wire:click=" setActiveGroupTeamL('create', 0)">Voltar</button>
                 </form>
-                <button class="createButton" wire:click=" setActiveGroupTeamL('create', 0)">Cancelar</button>
-            @elseif ($activeGroupTeamL == 'delete')
+
+            </div>
+
+
+        @elseif ($activeGroupTeamL == 'delete')
+
         @endif
 
+    </div>
+    <div class="containerRightTeam">
+        <h1>teste</h1>
     </div>
 
 
